82365c27d6fc5da5d23c0e9f81ec3ae68cfbd869
sanoopsadique <71060447+sanoopsadique@users.noreply.github.com>
2022-11-02 00:46:31 -0600

    build updated

--- container/build.sh
+++ container/build.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 if [ $# -eq 0 ]
   then
-    tag='latest'
+    echo Enter tag?
+    read tag
+    if [ tag == '']
+        then
+            tag='latest'
+    fi
   else
     tag=$1
 fi
 
 docker build -t sanoopsadique/smart:$tag .
+echo Do you want to pull the image to docker hub?
+read choice
+if [ choice == 'y']
+        then
+            docker push sanoopsadique/smart:$tag
+    
