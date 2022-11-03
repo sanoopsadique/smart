edbd37081d364b185559b1d0b9ab36b3bea2253c
sanoopsadique <71060447+sanoopsadique@users.noreply.github.com>
2022-11-03 00:46:57 -0600

    updateimage

---
+++ container/updateimage.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker rmi -f sanoopsadique/smart:latest
+docker pull sanoopsadique/smart:latest
--- container/watchtower.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
